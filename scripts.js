2c041f4d5427927ad51c12d6b1a4b5f5f4dce9ee
yarmajnr <yarmalight@yahoo.com>
2024-07-08 20:06:41 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    sidebar.classList.toggle('show');
+    overlay.classList.toggle('show');
+}
+
+function closeSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    sidebar.classList.remove('show');
+    overlay.classList.remove('show');
+}
